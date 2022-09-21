ba7e0d43dba155515297ecba39287f342d53f198
Hania-Abdul-Baseer <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2022-09-21 14:24:36 +0930

    timer now works for a set hour, min, and sec

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-let startingMinutes = 10;
-let time = startingMinutes*60;
+let startingHours = 2;
+let startingMinutes = 30;
+let startingSeconds = 3;
+let time = startingHours*60*60 + startingMinutes*60 + startingSeconds;
 
 const timerElement = document.getElementById('timer');
 
 setInterval(updateTimer, 1000);
 
 function updateTimer() {
-    const minutes = Math.floor(time/60);
+    let hours = Math.floor(time/3600); 
+
+    let temp = time - hours*60*60;
+    let minutes = Math.floor(temp/60);
+
     let seconds = time%60;
 
-    if(seconds < 10) seconds = '0'+seconds;
+    if(hours<10) hours = '0' + hours;
+    if(minutes<10) minutes = '0' + minutes;
+    if(seconds<10) seconds = '0'+seconds;
 
-    timerElement.innerHTML = `${minutes}:${seconds}`;
+    timerElement.innerHTML = `${hours}:${minutes}:${seconds}`;
     time--;
 }
