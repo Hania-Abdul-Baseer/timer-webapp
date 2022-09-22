a894cfc78cac11aa80387c12ead454c9970628c1
Hania-Abdul-Baseer <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2022-09-22 10:48:38 +0930

    users can input their own time and more basic functionalities were added

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-let startingHours = 2;
-let startingMinutes = 30;
-let startingSeconds = 3;
-let time = startingHours*60*60 + startingMinutes*60 + startingSeconds;
-
-const timerElement = document.getElementById('timer');
-
-setInterval(updateTimer, 1000);
+var isSetTimeoutRunning = false;
+var interval;
 
 function updateTimer() {
-    let hours = Math.floor(time/3600); 
+    if(isSetTimeoutRunning===true) return;
 
-    let temp = time - hours*60*60;
-    let minutes = Math.floor(temp/60);
+    const startingHours = document.getElementById('hours-input').value;
+    const startingMinutes = document.getElementById('minutes-input').value;
+    const startingSeconds = document.getElementById('seconds-input').value;
 
-    let seconds = time%60;
+    if(startingHours<0 || startingMinutes<0 || startingSeconds<0){
+        alert("Values must be equal to or greater than 0");
+        return;
+    }
+    if(startingMinutes>59){
+        alert("Minutes must be equal to or less than 59");
+        return;
+    }
+    if(startingSeconds>59){
+        alert("Seconds must be equal to or less than 59");
+        return;
+    }
 
-    if(hours<10) hours = '0' + hours;
-    if(minutes<10) minutes = '0' + minutes;
-    if(seconds<10) seconds = '0'+seconds;
+    let time = startingHours*60*60 + startingMinutes*60 + Number(startingSeconds);
+    isSetTimeoutRunning = true;
 
-    timerElement.innerHTML = `${hours}:${minutes}:${seconds}`;
-    time--;
+    interval = setInterval(function(){
+        var timerElement = document.getElementById('timer');
+
+        if(time<=-1) return;
+
+        let hours = Math.floor(time/3600); 
+        let temp = time - hours*60*60;
+        let minutes = Math.floor(temp/60);
+        let seconds = time - hours*60*60 - minutes*60;
+
+        if(hours<10) hours = '0'+hours;
+        if(minutes<10) minutes = '0'+minutes;
+        if(seconds<10) seconds = '0'+seconds;
+        
+        timerElement.innerHTML = `${hours}:${minutes}:${seconds}`;
+        time--;
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(interval);
+    isSetTimeoutRunning = false;
+}
+
+function resetTimer() {
+    stopTimer();
+    document.getElementById('timer').innerHTML = `00:00:00`;
 }
